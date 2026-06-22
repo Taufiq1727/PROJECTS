2bde9ca9a7848b587529d1b5cc84909f60250e1a
Taufiq <tm8023009@gmail.com>
2026-06-22 16:33:43 +0530

    Add listing routes and views for displaying all listings and listing details

--- WanderLust/app.js
+++ WanderLust/app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const port = 3014;
 const Listing = require("./models/listing");
-
+const path = require("path");
 
 main()
   .then(() => console.log("Connected to MongoDB"))
@@ -12,33 +12,51 @@ main()
 async function main() {
   await mongoose.connect("mongodb://localhost:27017/wanderlust");
 }
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Hi root");
 });
 
-app.get("/testListing", async (req, res) => {
-  try {
-    const sampleListing = new Listing({
-      title: "Sample Listing",
-      description: "This is a sample listing for testing purposes.",
-      image: {
-        filename: "listingimage",
-        url: "",
-      },
-      price: 100,
-      location: "Bangalore",
-      country: "India",
-    });
+app.get("/listings", async (req, res) => {
+   const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings})
+    
+  });
 
-    await sampleListing.save();
-    console.log("Sample listing saved to the database.");
-    res.send("Sample listing created and saved to the database.");
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Something went wrong while creating the listing.");
-  }
-});
+
+  //show route 
+  app.get("/listings/:id", async (req, res) => {
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{listing})
+  });
+
+  
+// app.get("/testListing", async (req, res) => {
+//   try {
+//     const sampleListing = new Listing({
+//       title: "Sample Listing",
+//       description: "This is a sample listing for testing purposes.",
+//       image: {
+//         filename: "listingimage",
+//         url: "",
+//       },
+//       price: 100,
+//       location: "Bangalore",
+//       country: "India",
+//     });
+
+//     await sampleListing.save();
+//     console.log("Sample listing saved to the database.");
+//     res.send("Sample listing created and saved to the database.");
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).send("Something went wrong while creating the listing.");
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
