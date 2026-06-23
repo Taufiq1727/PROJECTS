57b6925fac2786e4175f6c37d8baaae8915d9aba
Taufiq <tm8023009@gmail.com>
2026-06-23 19:03:30 +0530

    Day 73: Completed CRUD routes for project
    - Implemented create route
    - Added read and show functionality
    - Completed edit and update routes
    - Added delete route
    - Connected CRUD operations with MongoDB
    - Continued full-stack project development

--- WanderLust/app.js
+++ WanderLust/app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const port = 3014;
 const Listing = require("./models/listing");
 const path = require("path");
-
+const methodOverride = require("method-override");
 main()
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
@@ -16,6 +16,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.get("/", (req, res) => {
   res.send("Hi root");
 });
@@ -27,14 +28,49 @@ app.get("/listings", async (req, res) => {
   });
 
 
-  //show route 
-  app.get("/listings/:id", async (req, res) => {
-    const {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs",{listing})
-  });
-
   
+//new Route
+app.get("/listings/new", (req, res) => {
+res.render("listings/new.ejs");
+});
+//show route 
+app.get("/listings/:id", async (req, res) => {
+  const {id} = req.params;
+  const listing = await Listing.findById(id);
+   res.render("listings/show.ejs",{listing})
+});
+
+//create route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+  console.log(listing);
+});
+
+//Edit Route 
+app.get("/listings/:id/edit", async (req, res) => {
+  const {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs",{listing})
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  const {id} = req.params;
+  const listing = await Listing.findByIdAndUpdate(id, req.body.listing, {new:true});
+  res.redirect(`/listings/${listing._id}`);
+});
+
+//delete route
+app.delete("/listing/:id", async (req, res) => {
+  const {id} = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+});
+
+
+
 // app.get("/testListing", async (req, res) => {
 //   try {
 //     const sampleListing = new Listing({
