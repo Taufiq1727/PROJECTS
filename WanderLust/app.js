90b3c1b6b462bc6f5fa9db2e85b33714dd588530
Taufiq <tm8023009@gmail.com>
2026-07-02 23:11:07 +0530

    Day 74: Added project boilerplate, navbar and footer
    - Created reusable boilerplate layout
    - Implemented responsive navbar
    - Added footer component
    - Improved project structure and UI foundation

--- WanderLust/app.js
+++ WanderLust/app.js
@@ -5,6 +5,8 @@ const port = 3014;
 const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
 main()
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
@@ -17,6 +19,9 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
   res.send("Hi root");
 });
