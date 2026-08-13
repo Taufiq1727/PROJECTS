549d8fbd0b5f660fb24d70dc0b4ad279e6cafb63
Taufiq <tm8023009@gmail.com>
2026-08-13 19:10:21 +0530

    Add image handling to listings: update schema and views for optional image URLs

--- WanderLust/schema.js
+++ WanderLust/schema.js
@@ -4,6 +4,10 @@ module.exports.listingSchema = Joi.object({
     listing: Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
+        image: Joi.object({
+            url: Joi.string().allow("", null),
+            filename: Joi.string().allow("", null)
+        }),
         price: Joi.number().required().min(1),
         location: Joi.string().required(),
         country: Joi.string().required()
